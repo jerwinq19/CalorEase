5571c8e7c8cf4f796669c8c11b4fd433f21d161e
jerwin_nico <137849267+jerwinq19@users.noreply.github.com>
2025-05-04 09:20:48 +0800

    small changes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,11 +3,6 @@ const DashBoard = () => {
     return(
         <div>
             <h1 className="text-3xl text-center">This is the dash board</h1>
-<<<<<<< HEAD
-=======
-            <p>NIGGA</p>
-            <FoodTracker />
->>>>>>> bbb9ed3eb96bc2d3cb870ef3e1ff66c706e266a9
         </div>
     );
 };
